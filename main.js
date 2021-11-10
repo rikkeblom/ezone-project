c40b512b604bf369790e90cd57768d97bdf542c2
Filip Soudakov <fili4252@stud.kea.dk>
2021-11-10 09:40:57 +0100

    added the function to a module and exported to main.js

--- main.js
+++ main.js
@@ -1,22 +1,5 @@
 import "./style.scss";
 
-// import { filter } from "./modules/filter-quiz-toggle.js";
-
-// console.log(filter);
+import { hideAndUnhideFilterAndQuiz } from "./modules/filter-quiz-toggle";
 
 hideAndUnhideFilterAndQuiz();
-
-function hideAndUnhideFilterAndQuiz() {
-  const filterQuizRadioBtns = document.querySelectorAll(`input[name="filter-quiz-radio"]`);
-  filterQuizRadioBtns.forEach((btn) => {
-    btn.onclick = function () {
-      if (btn.value === "filter") {
-        document.querySelector(`section.quiz-container`).classList.add("hidden");
-        document.querySelector(`section.filter-container`).classList.remove("hidden");
-      } else {
-        document.querySelector(`section.filter-container`).classList.add("hidden");
-        document.querySelector(`section.quiz-container`).classList.remove("hidden");
-      }
-    };
-  });
-}
--- modules/filter-quiz-toggle.js
+++ modules/filter-quiz-toggle.js
@@ -1,1 +1,14 @@
-
+export function hideAndUnhideFilterAndQuiz() {
+  const filterQuizRadioBtns = document.querySelectorAll(`input[name="filter-quiz-radio"]`);
+  filterQuizRadioBtns.forEach((btn) => {
+    btn.onclick = function () {
+      if (btn.value === "filter") {
+        document.querySelector(`section.quiz-container`).classList.add("hidden");
+        document.querySelector(`section.filter-container`).classList.remove("hidden");
+      } else {
+        document.querySelector(`section.filter-container`).classList.add("hidden");
+        document.querySelector(`section.quiz-container`).classList.remove("hidden");
+      }
+    };
+  });
+}
