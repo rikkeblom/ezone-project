dc437cb591cd3c50420e274160753efdffb53a38
Filip Soudakov <fili4252@stud.kea.dk>
2021-11-10 15:10:22 +0100

    saved user info in local storage as object

--- modules/sign-up-form.js
+++ modules/sign-up-form.js
@@ -3,8 +3,23 @@
 //save form content in localStorage by clicking button
 export function saveUserInfoInLocalStorage(e) {
   e.preventDefault();
-  console.log("validated click");
   const form = document.querySelector("#first-modal form");
-  console.log(form.elements);
+  console.log(form.elements.username.value);
+  let userObject = {
+    username: "",
+    email: "",
+    password: "",
+  };
+
+  userObject.username = form.elements.username.value;
+  userObject.email = form.elements.email.value;
+  userObject.password = form.elements.password.value;
+
+  console.log(userObject);
+
+  //localStorage
+  const userLocalStorage = JSON.stringify(userObject);
+  localStorage.setItem("user", userLocalStorage);
 }
+
 //open next page
