014c915d8151b66fec1e715d04fd59f1ddb9f012
Filip Soudakov <fili4252@stud.kea.dk>
2021-11-10 09:32:48 +0100

    made a function to hide and unhide filter/quiz based on toggle selection

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 import "./style.scss";
+
+// import { filter } from "./modules/filter-quiz-toggle.js";
+
+// console.log(filter);
+
+hideAndUnhideFilterAndQuiz();
+
+function hideAndUnhideFilterAndQuiz() {
+  const filterQuizRadioBtns = document.querySelectorAll(`input[name="filter-quiz-radio"]`);
+  filterQuizRadioBtns.forEach((btn) => {
+    btn.onclick = function () {
+      if (btn.value === "filter") {
+        document.querySelector(`section.quiz-container`).classList.add("hidden");
+        document.querySelector(`section.filter-container`).classList.remove("hidden");
+      } else {
+        document.querySelector(`section.filter-container`).classList.add("hidden");
+        document.querySelector(`section.quiz-container`).classList.remove("hidden");
+      }
+    };
+  });
+}
