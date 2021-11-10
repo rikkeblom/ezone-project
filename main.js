a2b25d03c6ffa4ddf9b31af9ea7df4e0d61e2477
Filip Soudakov <fili4252@stud.kea.dk>
2021-11-10 14:45:33 +0100

    created module for form and made a function to preventDefault

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 import "./style.scss";
 
+//SIGN-UP FORM
+import { saveUserInfoInLocalStorage } from "./modules/sign-up-form";
+
+document.querySelector("#first-modal button[type=submit]").addEventListener("click", saveUserInfoInLocalStorage);
+
 import { hideAndUnhideFilterAndQuiz } from "./modules/filter-quiz-toggle";
 
 hideAndUnhideFilterAndQuiz();
---
+++ modules/sign-up-form.js
@@ -0,0 +1,10 @@
+//validate password (same as other)
+
+//save form content in localStorage by clicking button
+export function saveUserInfoInLocalStorage(e) {
+  e.preventDefault();
+  console.log("validated click");
+  const form = document.querySelector("#first-modal form");
+  console.log(form.elements);
+}
+//open next page
