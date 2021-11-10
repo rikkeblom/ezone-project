5bc0101009d2fea9409cee408bcd8b81003dba57
Filip Soudakov <fili4252@stud.kea.dk>
2021-11-10 11:58:33 +0100

    stored user selection of filters on click of submit button, and got filters stored in localStorage

--- main.js
+++ main.js
@@ -2,7 +2,13 @@ import "./style.scss";
 
 // FILTER
 import { hideAndUnhideFilterAndQuiz } from "./modules/filter-quiz-toggle";
-import { setupLocalStorageItem } from "./modules/filter";
+import { setupLocalStorageItem, readFilterButtons } from "./modules/filter";
+// import { setupLocalStorageItem } from "./modules/quiz";
 
 hideAndUnhideFilterAndQuiz();
 setupLocalStorageItem();
+getUserFilterSelection();
+
+function getUserFilterSelection() {
+  document.querySelector("form.filter button[type=submit]").addEventListener("click", readFilterButtons);
+}
--- modules/filter.js
+++ modules/filter.js
@@ -22,3 +22,25 @@ export function setupLocalStorageItem() {
   let filterString = JSON.stringify(filters);
   localStorage.setItem("filters", filterString);
 }
+
+function getLocalStorageItem() {
+  const filterString = localStorage.getItem("filters");
+  let filterObject = JSON.parse(filterString);
+  console.log(filterObject);
+}
+
+export function readFilterButtons(e) {
+  e.preventDefault();
+  getLocalStorageItem();
+  const selectedFilters = document.querySelectorAll(`input[name="interest"]:checked`);
+  if (selectedFilters.length > 0) {
+    selectedFilters.forEach((filter) => {
+      console.log("user chose:", filter.id);
+    });
+  } else {
+    console.log("no filters selected");
+  }
+
+  // let filterLocalStorage = JSON.stringify(filterObject);
+  // localStorage.setItem("filters", filterLocalStorage);
+}
